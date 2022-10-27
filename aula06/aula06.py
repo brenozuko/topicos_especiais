9b5766c9633f21f4284663f489dc169cffccd1e1
Breno Zukowski <brenozukotech@gmail.com>
2022-10-27 09:54:14 -0300

    Adiciona rename e rmdir

--- aula06/aula06.py
+++ aula06/aula06.py
@@ -1,11 +1,22 @@
 import os
+
 dir = os.path.join(os.sep, 'c:\\', 'temp')
 
-if(not os.path.exists(dir)):
+if (not os.path.exists(dir)):
     os.makedirs(dir)
 
-
 os.chdir(dir)
-for i in range(1,6):
-    os.mkdir(str(i))
 
+# for i in range(1,6):
+#     os.mkdir(str(i))
+
+
+# for i in os.listdir(dir):
+#     atual = os.path.join(os.sep, os.getcwd(), i)
+#     novo = os.path.join(os.sep, os.getcwd(), "dir_%s" % i)
+#     print(atual, novo)
+#     os.rename(atual, novo)
+
+
+for i in os.listdir(dir):
+    os.rmdir(str(i))
